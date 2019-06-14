de5ebf2a82d3f664a4436a418040ec409f7158c2
longht1989 <thanhlong.vn09@gmail.com>
2019-06-14 10:49:37 +0700

    update

--- js/app.js
+++ js/app.js
@@ -1,13 +1,21 @@
 // aminate fade in index section
 $(function() {
-    window.sr = ScrollReveal({ reset: true });
-    sr.reveal('.foo');
+    // window.sr = ScrollReveal({ reset: true });
+    // ScrollReveal.reveal('.foo');
+    var slideUp = {
+        delay: 100,
+        distance: '50%',
+        origin: 'bottom',
+        opacity: 0,
+        easing: 'ease-out'
+    };
+
+    ScrollReveal().reveal('.foo', slideUp);
     window.onscroll = function() { stickyHeader() };
 });
 
 function stickyHeader() {
     var posCover = $('#zone-cover').offset().top;
-    console.log(posCover);
     if (document.documentElement.scrollTop > posCover || document.body.scrollTop > posCover) {
         $('#site-header').addClass('affix');
     } else {
